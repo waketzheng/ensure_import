f44e86d6e84823b081f0251177500526594d7511
Waket Zheng <waketzheng@gmail.com>
2023-07-26 06:32:11 +0800

    save stage

--- ensure_import/__init__.py
+++ ensure_import/__init__.py
@@ -5,63 +5,83 @@ import subprocess
 import sys
 from contextlib import AbstractContextManager
 from pathlib import Path
+from typing import Optional
 
 
 class EnsureImport(AbstractContextManager):
     """Auto install modules if import error.
 
     Usage::
-        >>> for _ range(EnsureImport.retry):
+        >>> while EnsureImport.retry:
         ...     with EnsureImport(
         ...         multipart='python-multipart', dotenv='python-dotenv'
-        ...     ) as _m:
+        ...     ):
         ...         import six
         ...         import multipart
+        ...         import numpy as np
         ...         from dotenv import load_dotenv
         ...         # more imports ...
-        ...     if _m.ok:
-        ...         break
         ...
     """
 
+    mapping = {
+        "multipart": "python-multipart",
+        "dotenv": "python-dotenv",
+        "tortoise": "tortoise-orm",
+        "snap7": "python-snap7",
+    }
     retry = 30
+    last_one = ""
 
     def __init__(self, **kwargs):
         self.exception = None
         self._success = True
-        self.package_mapping = kwargs
+        self.package_mapping = dict(self.mapping, **kwargs)
 
     @property
     def ok(self) -> bool:
         return self._success
 
     def __exit__(self, exc_type, exc_value, traceback):
-        if isinstance(exc_value, (ImportError, ModuleNotFoundError)):
+        print("0" * 20, exc_type, exc_value, traceback)
+        if exc_value is None:
+            if self.retry == 1:
+                self.__class__.retry = 0
+            else:
+                self.__class__.retry = 1
+            print("=" * 20, exc_type, exc_value, traceback)
+        elif isinstance(exc_value, (ImportError, ModuleNotFoundError)):
+            print("+" * 20, exc_type, exc_value, traceback)
             self.exception = exc_value
             self._success = False
-            self.run()
-            return True
+            return self.run()
+        else:
+            print("-" * 20, exc_type, exc_value, traceback)
 
-    def run(self):
+    def run(self) -> Optional[bool]:
         e = self.exception
-        if "--no-install" in sys.argv:
-            raise e
         modules = re.findall(r"'([a-zA-Z][0-9a-zA-Z_-]+)'", str(e))
+        if not modules or "--no-install" in sys.argv:
+            raise e
+        if modules[-1] == self.last_one:
+            print(88888888888, self.last_one)
+            return False
         if mp := self.package_mapping:
             modules = [mp.get(i, i) for i in modules]
         if rc := self.install_and_extend_sys_path(*modules):
             sys.exit(rc)
+        return True
 
     @staticmethod
     def is_venv() -> bool:
-        """判断是否处于虚拟环境(也适用于poetry的)"""
+        """Whether in virtual environment(also work for poetry)"""
         if hasattr(sys, "real_prefix"):
             return True
         return hasattr(sys, "base_prefix") and sys.base_prefix != sys.prefix
 
     @staticmethod
     def run_and_echo(cmd: str) -> int:
-        print("-->\n", cmd, flush=True)
+        print("--> Executing shell command:\n", cmd, flush=True)
         return subprocess.call(cmd, shell=True)
 
     @staticmethod
@@ -69,7 +89,13 @@ class EnsureImport(AbstractContextManager):
         print(f"ERROR: failed to {action}")
 
     @classmethod
-    def install_and_extend_sys_path(cls, *packages) -> int:
+    def update_state(cls, module: str) -> None:
+        cls.last_one = module
+        if cls.retry > 0:
+            cls.retry -= 1
+
+    @classmethod
+    def install_and_extend_sys_path(cls, *packages: str) -> int:
         py = Path(sys.executable)
         depends = " ".join(packages)
         if not cls.is_venv():
@@ -81,9 +107,13 @@ class EnsureImport(AbstractContextManager):
                 py = p / "Scripts" / "python.exe"
             else:
                 py = p / "bin/python"
+            # cls.run_and_echo(f"{py} -m pip install --upgrade pip ensure_import")
+            cls.run_and_echo(f"{py} -m pip install --upgrade pip")
+            cls.run_and_echo(f"{py} -m pip install -e .")
             lib = list(p.rglob("site-packages"))[0]
             sys.path.append(lib.as_posix())
         if cls.run_and_echo(f"{py} -m pip install {depends}"):
             cls.log_error(f"install {depends}")
             return 2
+        cls.update_state(packages[-1])
         return 0
--- examples/main.py
+++ examples/main.py
@@ -3,17 +3,21 @@ from pathlib import Path
 
 from ensure_import import EnsureImport
 
-
-for _ in range(EnsureImport.retry):
-    with EnsureImport() as _m:
+count = 0
+print(f"{EnsureImport.retry = }, {EnsureImport.last_one=}")
+while EnsureImport.retry:
+    with EnsureImport():
         import uvicorn
         from fastapi import FastAPI
-    if _m.ok:
+    print(33333333, EnsureImport.retry, EnsureImport.last_one)
+    if count > 10:
         break
+    count += 1
+    print(f"{FastAPI = }")
 
 
 app = FastAPI(title=Path(__file__).parent.name)
 
 
-if __name__ == '__main__':
-    uvicorn.run('main:app')
+if __name__ == "__main__":
+    uvicorn.run("main:app")
