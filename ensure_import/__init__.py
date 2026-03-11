e68d06e5ae54321f003122d963cd378a2b7bdff3
Waket Zheng <waketzheng@gmail.com>
2026-03-12 00:26:47 +0800

    Bump version: 0.6.4 → 0.6.5

--- ensure_import/__init__.py
+++ ensure_import/__init__.py
@@ -30,7 +30,7 @@ if TYPE_CHECKING:
     else:
         from typing_extensions import Self
 
-__version__ = "0.6.4"
+__version__ = "0.6.5"
 logger = logging.getLogger(__name__)
 
 PathLike = str | Path
